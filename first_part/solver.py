b708652db2e695b6f5ed40c84abbb5d912e43223
floxo115 <flojak@gmx.net>
2025-04-24 08:03:32 +0200

    changed structure of project

--- first_part/solver.py
+++ first_part/solver.py
@@ -1,14 +1,7 @@
 from itertools import product
 from time import perf_counter
-from glob import glob
 
-import sys
-import os
-sys.path.append(os.path.abspath("../"))
-
-from dimacs_reader import DIMACSReader
-
-class Solver:
+class SimpleSolver:
     def __init__(self, num_variables, clauses, timeout=float("inf")):
         self.num_variables = num_variables
         self.clauses = clauses
@@ -37,24 +30,3 @@ class Solver:
                 self.model = assignment
                 return True
         return False
-
-def main():
-    files = glob("../test-formulas/*.in")
-    for file in files:
-        print(f"solving file {file}")
-        dimacs_reader = DIMACSReader()
-        dimacs_reader.read(file)
-
-        solver = Solver(dimacs_reader.num_vars, dimacs_reader.clauses, timeout=5)
-        try:
-            is_sat = solver.solve()
-            print(is_sat)
-            print(solver.model)
-        except TimeoutError:
-            print("timeout")
-        print()
-
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,27 @@
+from glob import glob
+
+
+from dimacs_reader import DIMACSReader
+from first_part import SimpleSolver
+
+
+def main():
+    files = glob("./test-formulas/*.in")
+    for file in files:
+        print(f"solving file {file}")
+        dimacs_reader = DIMACSReader()
+        dimacs_reader.read(file)
+
+        solver = SimpleSolver(dimacs_reader.num_vars, dimacs_reader.clauses, timeout=5)
+        try:
+            is_sat = solver.solve()
+            print(is_sat)
+            print(solver.model)
+        except TimeoutError:
+            print("timeout")
+        print()
+
+
+if __name__ == "__main__":
+    print("starting solver")
+    main()
