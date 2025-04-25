f5aff81fc0192c31db10c909db4bd81d14b10076
floxo115 <flojak@gmx.net>
2025-04-25 13:19:22 +0200

    started to worke on dpll with watch literals

---
+++ second_part/clause_test.py
@@ -0,0 +1,113 @@
+import pytest
+from .solver import Clause
+
+def test_init_clause():
+    clause = Clause([-1,2,-6, 10])
+    assert clause.literals == [-1,2,-6, 10]
+    assert clause.watch_pointer1 == 0
+    assert clause.watch_pointer2 == 1
+
+def test_init_clause_with_one_literal():
+    clause = Clause([-1])
+    assert clause.literals == [-1]
+    assert clause.watch_pointer1 == 0
+    assert clause.watch_pointer2 == 0
+
+def test_init_clause_with_no_literal():
+    with pytest.raises(ValueError):
+        Clause([])
+
+def test_is_sat_with_true_assignment():
+    clause = Clause([-1,2,-6, 10])
+    assignment = {1:True, 2: False, 6: False}
+    assert clause.is_sat(assignment)
+
+def test_is_sat_with_false_assignment():
+    clause = Clause([-1,2,-6, 10])
+    assignment = {1:True, 2: False, 6: True}
+    assert not clause.is_sat(assignment)
+
+def test_is_sat_with_no_assignment():
+    clause = Clause([-1,2,-6, 10])
+    assignment = {}
+    assert not clause.is_sat(assignment)
+
+def test_watched_literals_with_unit_clause():
+    clause = Clause([-1])
+    assert clause.watch_pointer1 == clause.watch_pointer2 == 0
+
+def test_set_watched_literals_no_assignment_to_them():
+    clause = Clause([-1,2,-6, 10])
+    clause.watch_pointer1 = 2
+    clause.watch_pointer2 = 1
+    clause.set_watchers({1: True, 10: False},  10)
+    assert clause.watch_pointer1 == 2
+    assert clause.watch_pointer2 == 1
+
+def test_set_watched_literals_true_assignment():
+    clause = Clause([-1,2,-6, 10])
+    clause.watch_pointer1 = 2
+    clause.watch_pointer2 = 1
+    clause.set_watchers({1: True, 10: False, 2: True},  2)
+    # they swap because watch pointer 1 can only point to new variable assignment
+    assert clause.watch_pointer1 == 1
+    assert clause.watch_pointer2 == 2
+
+    clause = Clause([-1,2,-6, 10])
+    clause.watch_pointer1 = 2
+    clause.watch_pointer2 = 1
+    clause.set_watchers({1: True, 10: False, 6: False},  6)
+    # nothing happens
+    assert clause.watch_pointer1 == 2
+    assert clause.watch_pointer2 == 1
+
+def test_set_watched_literals_false_assignment_with_open_literals():
+    clause = Clause([-1,2,-6, 10])
+    clause.watch_pointer1 = 2
+    clause.watch_pointer2 = 1
+    clause.set_watchers({6:False},  6)
+    # watch pointer has to go to new position 0
+    assert clause.watch_pointer1 == 0
+    assert clause.watch_pointer2 == 1
+
+    clause = Clause([-1,2,-6, 10])
+    clause.watch_pointer1 = 2
+    clause.watch_pointer2 = 1
+    clause.set_watchers({1: False, 6:False},  6)
+    # watch pointer has to go to new position 3
+    assert clause.watch_pointer1 == 3
+    assert clause.watch_pointer2 == 1 
+
+def test_bcp_with_unit_clause():
+    clause = Clause([-1])
+    assert clause.bcp({2: True}) == (1, False)
+
+def test_bcp_with_pointer_to_true():
+    clause = Clause([-1,2,-6, 10])
+    clause.set_watchers({1:False, 2: True}, 2)
+    bcp_res = clause.bcp({1:False, 2: True})
+    # if watch pointer points to a True literal nothing should happen
+    assert bcp_res is None
+
+def test_bcp_with_pointer_to_false():
+    clause = Clause([-1,2,-6, 10])
+    clause.set_watchers({1:False, 2: False}, 2)
+    bcp_res = clause.bcp({1:False, 2: False})
+    assert bcp_res is None
+
+def test_bcp_with_pointer_unassigned_and_other_pointer_false_and_rest_false():
+    clause = Clause([-1,2,-6, 10])
+    clause.set_watchers({1:False, 2: False, 10: False}, 2)
+    bcp_res = clause.bcp({1:False, 2: False, 10: False})
+    assert bcp_res == (6, False)
+
+def test_bcp_with_pointer_unassigned_and_other_pointer_false_and_rest_has_one_true():
+    clause = Clause([-1,2,-6, 10])
+    clause.set_watchers({1:False, 2: False, 10: True}, 2)
+    bcp_res = clause.bcp({1:False, 2: False, 10: True})
+    assert bcp_res is None
+
+
+
+
+    
---
+++ second_part/solver.py
@@ -0,0 +1,101 @@
+from typing import Iterable, Dict
+
+
+class Clause:
+    def __init__(self, literals: Iterable[int]):
+        if len(literals) == 0:
+            raise ValueError("literals can not be empty")
+        self.literals = list(literals)
+
+        self.watch_pointer1 = 0
+        self.watch_pointer2 = 1 if len(self.literals) > 1 else 0
+
+    def is_sat(self, assignment: Dict[int, bool]) -> bool:
+        for literal in self.literals:
+            if abs(literal) in assignment.keys():
+                val = assignment[abs(literal)]
+                if literal > 0 and val or literal < 0 and not val:
+                    return True
+
+        return False
+
+    def set_watchers(self, assignment: Dict[int, bool], new_variable: int):
+        if new_variable <= 0:
+            raise ValueError("new variable can not be smaller than 1")
+        if new_variable not in assignment.keys():
+            raise ValueError("new variable has to be in assignment")
+
+        # if the literal is already unit, we have do nothing
+        if self.watch_pointer1 == self.watch_pointer2:
+            return
+
+        # if the new variable is watched by watch pointer 2 we swap the pointers
+        # so that always watch pointer 1 points to the new variable if any watch pointer
+        # points to it at all
+        if new_variable == abs(self.literals[self.watch_pointer2]):
+            self.watch_pointer1, self.watch_pointer2 = (
+                self.watch_pointer2,
+                self.watch_pointer1,
+            )
+
+        # if no watch pointer points to the new variable do nothing and return
+        if new_variable != abs(self.literals[self.watch_pointer1]):
+            return
+
+        # lit_val gives us the value of the literal watched by watch pointer 1 under the current assignment
+        lit_polarity = -1 if self.literals[self.watch_pointer1] < 0 else 1
+        lit_val = lit_polarity * assignment[abs(self.literals[self.watch_pointer1])]
+
+        # if the literal value pointed by watch pointer 1 is becoming True: do nothing
+        if lit_val:
+            return
+
+        # from here on we assume that the new variable assignment makes the literal
+        # watched by watch pointer 1 False
+
+        # and because of that we need to find a new non assigned and non non watched literal
+        for i, literal in enumerate(self.literals):
+            if abs(literal) not in assignment.keys() and self.watch_pointer2 != i:
+                self.watch_pointer1 = i
+                break
+
+    def bcp(self, assignment: Dict[int, bool]):
+        # if the clause is a unit clause we just return the variable and an assignment
+        # that makes the literal True
+        if self.watch_pointer1 == self.watch_pointer2:
+            lit_polarity = -1 if self.literals[self.watch_pointer1] < 0 else 1
+            return abs(self.literals[self.watch_pointer1]), lit_polarity > 0
+
+        # it is assumed that the watch pointers were set with set_watch pointers before
+        # if the second watch pointer's literal is True or unassigned we can not infer anything
+        if (
+            abs(self.literals[self.watch_pointer2]) not in assignment.keys()
+            or assignment[abs(self.literals[self.watch_pointer2])]
+        ):
+            return None
+
+        # if the first watch pointer points to a literal that is assigned also do nothing
+        if abs(self.literals[self.watch_pointer1]) in assignment.keys():
+            return None
+
+        # now we check if there are still unassigned and unwatched literals in the clause.
+        # If not the literal watched by watch pointer 1 has to be True.
+        for i, literal in enumerate(self.literals):
+            if i in [self.watch_pointer1, self.watch_pointer2]:
+                continue
+            elif abs(literal) not in assignment.keys():
+                return None
+
+            # if the current literals is true under the assignment then we can infer nothing
+            lit_polarity = -1 if literal < 0 else 1
+            is_assignment_true = 1 if assignment[abs(literal)] > 0 else -1
+            if lit_polarity * is_assignment_true > 0:
+                return None
+
+        # lit_val gives us the value of the literal watched by watch pointer 1 under the current assignment
+        lit_polarity = -1 if self.literals[self.watch_pointer1] < 0 else 1
+        return abs(self.literals[self.watch_pointer1]), lit_polarity > 0
+
+
+class DLPPSolver:
+    pass
